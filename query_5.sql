10215d166dffbbd581baee585adf995d5ab79b5b
Charles <chasb96@gmail.com>
2017-04-24 23:44:54 -0500

    Fixed query 5.

--- query_5.sql
+++ query_5.sql
@@ -2,7 +2,14 @@ use Cinema;
 
 SELECT m.MovieID, m.Title
 FROM Movie m
-WHERE (SELECT a.Available_Seats - (SELECT count(*) FROM Ticket WHERE ShowingID = s.ShowingID)
+WHERE (SELECT a.Available_Seats - ((SELECT t.ShowingID, SUM(od.No_of_Tickets) as No_of_Tickets
+			FROM OrderDetail od
+				JOIN [Order] o
+					ON od.OrderID = o.OrderID
+				JOIN Ticket t
+					ON t.TicketID = od.TicketID
+			WHERE t.ShowingID = s.ShowingID
+			GROUP BY t.ShowingID))
 		FROM Showing s
 			JOIN Auditorium a
 				ON s.AuditoriumID = a.AuditoriumID
