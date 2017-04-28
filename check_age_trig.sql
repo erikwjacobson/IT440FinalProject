3322184a747158a93254bb779c084667cc64a37c
Erik Jacobson <erik.jacobson@b507.us>
2017-04-28 14:45:16 -0500

    Moved one trigger's functionality to another

--- check_age_trig.sql
+++ check_age_trig.sql
@@ -2,9 +2,10 @@
 The following file covers the following constraints:
 	'A Customer needs to be older than 13 to order a ticket.'
 	'A Customer that is younger than 17 is not allowed to purchase a ticket for an R-rated movie.'
+	'It is not allowed sell more tickets than there are seats available for a showing.'
 */
 
-CREATE TRIGGER checkAge
+CREATE TRIGGER checkOrder
 ON OrderDetail
 INSTEAD OF INSERT
 AS
@@ -16,7 +17,12 @@ BEGIN
 	DECLARE @custBirthdate DATE;
 	DECLARE @age INT;
 	DECLARE @movieRatingID INT;
+	DECLARE @showingID INT;
 	DECLARE @noTickets INT;
+	DECLARE @totalSeats INT;
+	DECLARE @seatsLeft INT;
+	DECLARE @seatsBeingPurchased INT;
+	DECLARE @ticketsAlreadyPurchased INT;
 
 	/* Set Variables */
 	SET @ticketID = (SELECT TicketID FROM INSERTED);
@@ -32,13 +38,31 @@ BEGIN
 											 WHERE S.ShowingID = (SELECT ShowingID
 																  FROM Ticket T
 																  WHERE T.TicketID = @ticketID)));
-
-	/* LOGIC FOR 'A Customer needs to be older than 13 to order a ticket.' */
+	SET @showingID = (SELECT ShowingID FROM Ticket T WHERE T.TicketID = @ticketID);
+	SET @totalSeats = (SELECT Available_Seats 
+						FROM Auditorium 
+						WHERE AuditoriumID = (SELECT AuditoriumID 
+												FROM Showing S 
+												WHERE S.ShowingID = @showingID));
+	SET @seatsBeingPurchased = (SELECT No_of_Tickets FROM INSERTED);
+	SET @ticketsAlreadyPurchased = (SELECT SUM(No_of_Tickets)
+									  FROM OrderDetail OD 
+										JOIN Ticket T 
+									  ON OD.TicketID = T.TicketID
+										JOIN Showing S
+									  ON T.ShowingID = S.ShowingID
+										JOIN Auditorium A
+									  ON S.AuditoriumID = A.AuditoriumID
+									  WHERE S.ShowingID = 1);
+	SET @seatsLeft = @totalSeats - (@ticketsAlreadyPurchased + @seatsBeingPurchased);
+	/* LOGIC FOR ALL CONSTRAINTS */
 	BEGIN
 		IF @age < 13
 			RAISERROR('You have to be at least 13 years old to order a ticket',13,13);
 		ELSE IF @movieRatingID > 3 AND @age < 17
 			RAISERROR('You have to be at least 17 years old to order a ticket with a rating of R',13,13);
+		ELSE IF @seatsLeft < 0
+			RAISERROR('You have ordered too many tickets.',13,13);
 		ELSE
 			INSERT INTO OrderDetail VALUES(@orderID,@ticketID,@noTickets);
 	END
@@ -63,10 +87,17 @@ BEGIN TRANSACTION;
 	EXEC orderTicket @numberOfTickets = 3, @customerID = 7, @date = @varDate2, @ticketID = 3;
 	/* Customer is only 12 years of age */
 COMMIT TRANSACTION;
-SELECT * FROM Movie where MovieID = 1;
+
 BEGIN TRANSACTION;
 	DECLARE @varDate2 DATE;
 	SET @varDate2 = GETDATE();
 	EXEC orderTicket @numberOfTickets = 3, @customerID = 8, @date = @varDate2, @ticketID = 8;
 	/* Customer is only 16 years of age */
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+	DECLARE @varDate2 DATE;
+	SET @varDate2 = GETDATE();
+	EXEC orderTicket @numberOfTickets = 200, @customerID = 8, @date = @varDate2, @ticketID = 8;
+	/* Customer ordered too many tickets */
 COMMIT TRANSACTION;
