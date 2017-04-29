850ade921331ebbe79401b71962869be5759ca48
Charles <chasb96@gmail.com>
2017-04-28 19:43:42 -0500

    Updated constraint 7 to include constraint 4.

--- constraint_7.sql
+++ constraint_7.sql
@@ -1,7 +1,7 @@
 use Cinema
 GO
 
-CREATE TRIGGER assertShowingDate on Showing
+CREATE TRIGGER assertValidShowing on Showing
 INSTEAD OF INSERT, UPDATE
 AS 
 	BEGIN
@@ -10,7 +10,24 @@ AS
 		SELECT @releaseDate = ReleaseDate FROM Movie WHERE MovieID IN (SELECT MovieID FROM inserted);
 		
 		IF @releaseDate < (SELECT Date FROM inserted)
-			INSERT INTO Showing SELECT MovieID, AuditoriumID, Date, Time FROM inserted
+		BEGIN
+			DECLARE @movieCount INT
+			DECLARE @datetime DATETIME
+
+			SELECT @datetime = CAST(DATE AS DATETIME) + CAST(TIME AS DATETIME) FROM inserted;
+
+			SELECT @movieCount = COUNT(*) 
+			FROM Showing s
+				JOIN Movie m
+					ON s.MovieID = m.MovieID
+			WHERE (CAST(DATE AS DATETIME) + CAST(TIME AS DATETIME)) <= @datetime
+				AND DATEADD(MINUTE, m.Runtime, CAST(DATE AS DATETIME) + CAST(TIME AS DATETIME)) >= @datetime;
+
+			IF @movieCount <= 0
+				INSERT INTO Showing SELECT MovieID, AuditoriumID, Date, Time FROM inserted
+			ELSE 
+				RAISERROR('Cannot show a movie during another movie', 13, 13);
+		END
 		ELSE 
 			RAISERROR('Cannot be showing before release date', 13, 13);
 	END
@@ -28,7 +45,7 @@ BEGIN TRAN testInsertShowing
 	GO
 ROLLBACK TRAN testInsertShowing
 
--- Failure Case --
+-- Failure Case: showing before release --
 BEGIN TRAN testInsertShowing
 	INSERT INTO Movie (Title, ReleaseDate, Runtime, RatingID) VALUES (
 		'Testing',
@@ -41,6 +58,26 @@ BEGIN TRAN testInsertShowing
 		@@IDENTITY,
 		(SELECT TOP 1 AuditoriumID FROM Auditorium ORDER BY RAND()),
 		'1970-01-01',
+		CONVERT(Time, GETDATE())
+	);
+ROLLBACK TRAN testInsertShowing
+
+-- Failure Case: two movies at the same time --
+BEGIN TRAN testInsertShowing
+	DECLARE @auditorium INT;
+	SELECT TOP 1 @auditorium = AuditoriumID FROM Auditorium ORDER BY RAND();
+	
+	INSERT INTO Showing (MovieID, AuditoriumID, Date, Time) VALUES (
+		(SELECT TOP 1 MovieID FROM Movie ORDER BY RAND()),
+		@auditorium,
+		CONVERT(Date, GETDATE()),
+		CONVERT(Time, GETDATE())
+	);
+
+	INSERT INTO Showing (MovieID, AuditoriumID, Date, Time) VALUES (
+		(SELECT TOP 1 MovieID FROM Movie ORDER BY RAND()),
+		@auditorium,
+		CONVERT(Date, GETDATE()),
 		CONVERT(Time, GETDATE())
 	);
 ROLLBACK TRAN testInsertShowing
